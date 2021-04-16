1befde779e32e24d009909cd7c4c6eacd21e0dc9
kouharakouki <koki.kohara916@gmail.com>
2021-04-16 07:33:40 +0000

    add profile-image-preview

--- Gemfile
+++ Gemfile
@@ -65,3 +65,5 @@ gem "refile", require: "refile/rails", github: 'manfe/refile'
 gem "refile-mini_magick"
 gem 'kaminari','~> 1.2.1'
 gem "jquery-rails"
+gem 'bootstrap', '~> 4.5'
+gem 'font-awesome-sass', '~> 5.13'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
@@ -33,5 +35,15 @@ $(function(){
           $(".image-item").attr('src', e.target.result);
       }
       reader.readAsDataURL(e.target.files[0]);
+  });
+})
+
+$(function(){
+  $('#user_profile_image').on('change', function (e) {
+    var reader = new FileReader();
+    reader.onload = function (e) {
+      $(".image-item").attr('src', e.target.result);
+    }
+    reader.readAsDataURL(e.target.files[0]);
   });
 })
