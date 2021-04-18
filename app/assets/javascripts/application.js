468424d0fbe7accdee07147bfdf948165f973fea
kouharakouki <koki.kohara916@gmail.com>
2021-04-18 00:35:40 +0000

    Finish ~Chapter9

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,35 +18,12 @@
 
 //= require_tree .
 
-
-// $(function(){
-//   $('.item-image').on('change', function (e) {
-//     var reader = new FileReader();
-//     reader.onload = function (e) {
-//       $(".item-image").attr('src', e.target.result);
-//     }
-//     reader.readAsDataURL(e.target.files[0]);
-//   })
-// })
-
-
-  // $(function () {
-  //   lightbox.option({
-  //     'alwaysShowNavOnTouchDevices': false,
-  //     'albumLabel': 'ギャラリー： %1 of %2',
-  //     'disableScrolling': false,
-  //     'fadeDuration': 600,
-  //     'fitImagesInViewport': true,
-  //     'imageFadeDuration': 600,
-  //     'maxWidth': 400,
-  //     'maxHeight': 400,
-  //     'positionFromTop': 50,
-  //     'resizeDuration': 700,
-  //     'showImageNumberLabel': true,
-  //     'wrapAround': false,
-  //   });
-  // });
-
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+//= require rails-ujs
+//= require activestorage
+//= require_self
 
 $(function(){
   $('#post_image_image').on('change', function (e) {
